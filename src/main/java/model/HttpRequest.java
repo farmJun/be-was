bfc211d96c99f3aece377ecdc57aa2aa160199a6
박준영 <wnsdud6969@naver.com>
2026-01-03 00:00:00 +0900

    feat(RequestParser): 동적 요청을 처리하기 위해 query parameter 추출 함수 추가

--- src/main/java/model/HttpRequest.java
+++ src/main/java/model/HttpRequest.java
@@ -8,22 +8,38 @@ public class HttpRequest {
     private final String path;
     private final String httpVersion;
     private final Map<String, String> headers;
+    private final Map<String, String> parameters;
     private final String body;
+    private final boolean isStatic;
 
-    public HttpRequest(String httpMethod, String path, String httpVersion, Map<String, String> headers, String body) {
+    public HttpRequest(String httpMethod, String path, String httpVersion, Map<String, String> headers, Map<String, String> parameters, String body, boolean isStatic) {
         this.httpMethod = httpMethod;
         this.path = path;
         this.httpVersion = httpVersion;
         this.headers = headers;
+        this.parameters = parameters;
         this.body = body;
+        this.isStatic = isStatic;
     }
 
     public String getPath() {
         return path;
     }
 
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
     public String getHttpVersion() {
         return httpVersion;
+    }
+
+    public boolean isStatic() {
+        return isStatic;
+    }
+
+    public Map<String, String> getParameters() {
+        return parameters;
     }
 
     @Override
@@ -33,6 +49,17 @@ public class HttpRequest {
         sb.append("\t").append(httpMethod).append(" ")
                 .append(path).append(" ")
                 .append(httpVersion).append("\n");
+
+        if (parameters != null && !parameters.isEmpty()) {
+            sb.append("\tParameters:\n");
+            for (Map.Entry<String, String> entry : parameters.entrySet()) {
+                sb.append("\t\t")
+                        .append(entry.getKey())
+                        .append(" = ")
+                        .append(entry.getValue())
+                        .append("\n");
+            }
+        }
 
         sb.append("\tHeaders:\n");
         for (Map.Entry<String, String> entry : headers.entrySet()) {
