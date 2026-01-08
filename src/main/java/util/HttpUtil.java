961c6898c18d262f7f092b4267a8a0d959a8f35c
박준영 <wnsdud6969@naver.com>
2026-01-08 15:31:14 +0900

    feat(HttpUtil): 콘텐츠 타입 enum 도입으로 getContentType() 제거, 로그인 상태인지 확인하는 isLoggedIn() 추가

--- src/main/java/util/HttpUtil.java
+++ src/main/java/util/HttpUtil.java
@@ -1,27 +1,13 @@
 package util;
 
+import db.Database;
+import http.request.HttpRequest;
+import model.User;
+
 import java.security.SecureRandom;
 import java.util.Base64;
 
 public class HttpUtil {
-    public static String getContentType(String path) {
-        if (path.endsWith(".html")) {
-            return "text/html;charset=utf-8";
-        } else if (path.endsWith(".css")) {
-            return "text/css;charset=utf-8";
-        } else if (path.endsWith(".js")) {
-            return "application/javascript";
-        } else if (path.endsWith(".png")) {
-            return "image/png";
-        } else if (path.endsWith(".jpg") || path.endsWith(".jpeg")) {
-            return "image/jpeg";
-        } else if (path.endsWith(".svg")) {
-            return "image/svg+xml";
-        } else if (path.endsWith(".ico")) {
-            return "image/vnd.microsoft.icon";
-        }
-        return "application/octet-stream";
-    }
 
     public static String generateRandomSessionId() {
         SecureRandom secureRandom = new SecureRandom();
@@ -32,5 +18,11 @@ public class HttpUtil {
         return Base64.getUrlEncoder()
                 .withoutPadding()
                 .encodeToString(randomBytes);
+    }
+
+    public static boolean isLoggedIn(HttpRequest httpRequest) {
+        User user = Database.findUserBySessionId(httpRequest.getSessionId());
+
+        return user != null;
     }
 }
