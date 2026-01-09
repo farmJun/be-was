7993f5cdb7e5b9cf30128e309e5739179cd48c06
박준영 <wnsdud6969@naver.com>
2026-01-09 18:40:41 +0900

    remove(HttpStatus): 사용하지 않는 HTTP status 제거

--- src/main/java/http/HttpStatus.java
+++ src/main/java/http/HttpStatus.java
@@ -3,24 +3,9 @@ package http;
 public enum HttpStatus {
     // 2xx Success
     OK(200, "OK"),
-    CREATED(201, "Created"),
-    NO_CONTENT(204, "No Content"),
 
     // 3xx Redirection
-    FOUND(302, "Found"),
-    SEE_OTHER(303, "See Other"),
-
-    // 4xx Client Error
-    BAD_REQUEST(400, "Bad Request"),
-    UNAUTHORIZED(401, "Unauthorized"),
-    FORBIDDEN(403, "Forbidden"),
-    NOT_FOUND(404, "Not Found"),
-    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
-
-    // 5xx Server Error
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
-    NOT_IMPLEMENTED(501, "Not Implemented"),
-    SERVICE_UNAVAILABLE(503, "Service Unavailable");
+    FOUND(302, "Found");
 
     private int statusCode;
     private String message;
