ae56ef41599f4f7f5d5fde7ce7d95150d5b32e0c
박준영 <wnsdud6969@naver.com>
2026-01-09 18:46:31 +0900

    feat(HttpUtil): 특정 path에 대한 접근 권한을 확인하는 메서드 추가

--- src/main/java/util/HttpUtil.java
+++ src/main/java/util/HttpUtil.java
@@ -1,13 +1,21 @@
 package util;
 
 import db.Database;
+
 import http.request.HttpRequest;
+
 import model.User;
 
 import java.security.SecureRandom;
+
 import java.util.Base64;
+import java.util.Set;
 
 public class HttpUtil {
+
+    private static final Set<String> LOGIN_REQUIRED_PATHS = Set.of(
+            "/mypage/index.html"
+    );
 
     public static String generateRandomSessionId() {
         SecureRandom secureRandom = new SecureRandom();
@@ -18,6 +26,14 @@ public class HttpUtil {
         return Base64.getUrlEncoder()
                 .withoutPadding()
                 .encodeToString(randomBytes);
+    }
+
+    public static boolean isUnauthorized(HttpRequest request) {
+        return isLoginRequiredPath(request.getStartLine().getPath()) && !isLoggedIn(request);
+    }
+
+    private static boolean isLoginRequiredPath(String path) {
+        return LOGIN_REQUIRED_PATHS.contains(path);
     }
 
     public static boolean isLoggedIn(HttpRequest httpRequest) {
