1ec4af622c6c318440ddbc417160c2ad140a6b3e
박준영 <wnsdud6969@naver.com>
2026-01-13 20:03:19 +0900

    feat(ArticleBusiness): 게시글 관련 로직을 처리하는 ArticleBusiness 추가 및 게시글 추가 구현

---
+++ src/main/java/business/ArticleBusiness.java
@@ -0,0 +1,26 @@
+package business;
+
+import db.Database;
+
+import http.request.HttpRequest;
+import http.response.HttpResponse;
+
+import model.Article;
+import model.User;
+
+public class ArticleBusiness {
+
+    public HttpResponse createArticle(HttpRequest httpRequest) {
+        String content = httpRequest.getBody().get("content");
+        User user = Database.findUserBySessionId(httpRequest.getSessionId());
+
+        if (user == null) {
+            throw new IllegalStateException("유저 없음");
+        }
+
+        Article article = new Article(user.getUserId(), content);
+        Database.addArticle(article);
+
+        return HttpResponse.redirect(httpRequest, "/index.html");
+    }
+}
