86c74a6ddd1695e43891f1dc8a5ff63cfd1f4900
박준영 <wnsdud6969@naver.com>
2026-01-13 20:08:19 +0900

    feat(RequestRouter): 요청을 관리하는 mappings의 자료구조 변경 및 그에 따른 메서드 수정

--- src/main/java/http/request/RequestRouter.java
+++ src/main/java/http/request/RequestRouter.java
@@ -1,33 +1,47 @@
 package http.request;
 
 import business.Business;
+import business.ArticleBusiness;
 import business.UserBusiness;
 
 import model.RouteKey;
+import model.RouteResult;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class RequestRouter {
 
-    private static final Map<RouteKey, Business> mappings = new HashMap<>();
+    private static final Map<String, Map<String, Business>> mappings = new HashMap<>();
 
     static {
         UserBusiness userBusiness = new UserBusiness();
-
-        mappings.put(
-                new RouteKey("POST", "/user/create"),
-                userBusiness::signUp
-        );
-
-        mappings.put(
-                new RouteKey("POST", "/user/login"),
-                userBusiness::login
-        );
+        ArticleBusiness articleBusiness = new ArticleBusiness();
+        register("/user/create", "POST", userBusiness::signUp);
+        register("/user/login", "POST", userBusiness::login);
+        register("/article/create", "POST", articleBusiness::createArticle);
     }
 
-    public static Business getHandler(HttpRequest request) {
-        return mappings.get(new RouteKey(request.getStartLine().getHttpMethod(), request.getStartLine().getPath()));
+    public static RouteResult route(RouteKey routeKey) {
+        Map<String, Business> methodMap = mappings.get(routeKey.getPath());
+
+        if (methodMap == null) {
+            return new RouteResult(null, false, Set.of());
+        }
+
+        Business handler = methodMap.get(routeKey.getMethod());
+
+        if (handler == null) {
+            return new RouteResult(null, true, Set.of());
+        }
+
+        return new RouteResult(handler, true, methodMap.keySet());
+    }
+
+    private static void register(String path, String httpMethod, Business business) {
+        mappings.computeIfAbsent(path, p -> new HashMap<>())
+                .put(httpMethod, business);
     }
 
 }
