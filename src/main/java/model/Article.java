25b1bd77d9436bab8d983d28241eef2cfe15cca9
박준영 <wnsdud6969@naver.com>
2026-01-14 18:36:34 +0900

    feat(Article): 게시글에 이미지 경로 추가

--- src/main/java/model/Article.java
+++ src/main/java/model/Article.java
@@ -5,10 +5,20 @@ public class Article {
     private Long id;
     private String userId;
     private String content;
+    private String imagePath;
 
-    public Article(String userId, String content) {
+    public Article(String userId, String content, String imagePath) {
         this.userId = userId;
         this.content = content;
+        this.imagePath = imagePath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public String getContent() {
+        return content;
     }
 
     public void setId(Long id) {
