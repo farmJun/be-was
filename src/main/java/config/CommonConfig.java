0a9ac6f15f677249f1b4a6289333e01ddc738605
박준영 <wnsdud6969@naver.com>
2026-01-15 15:15:58 +0900

    feat(CommonConfig): 이미지 업로드 폴더 추가

--- src/main/java/config/CommonConfig.java
+++ src/main/java/config/CommonConfig.java
@@ -2,4 +2,5 @@ package config;
 
 public class CommonConfig {
     public static final String baseDirectory = System.getProperty("user.dir") + "/src/main/resources/static";
+    public static final String UPLOAD_DIR = CommonConfig.baseDirectory + "/asset";
 }
