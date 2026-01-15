599b420447f0e4276ef8f639dc544776ee3599a6
박준영 <wnsdud6969@naver.com>
2026-01-15 17:11:54 +0900

    feat(JdbcConnection): h2 db 연결을 위한 JdbcConnection 추가

---
+++ src/main/java/db/JdbcConnection.java
@@ -0,0 +1,23 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnection {
+    private static final String URL = "jdbc:h2:tcp://localhost/~/web-server";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+
+    static {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
